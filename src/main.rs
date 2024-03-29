55297259f1b3c57a19012001a920f6a3d85fd0bf
alexcloudstar <alexcloudstar@gmail.com>
2024-03-29 18:39:48 +0200

    feat: add clap & args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+
+struct Args {
+    #[arg(long)]
+    from: String,
+
+    #[arg(long)]
+    to: String,
+
+    #[arg(long)]
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+     let args = Args::parse();
+
+    println!("{:?}", args);
+
 }
