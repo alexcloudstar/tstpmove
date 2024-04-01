91e661946a8ece947d1dc601acc1cfd011718a67
alexcloudstar <alexcloudstar@gmail.com>
2024-04-01 17:45:07 +0300

    fix: extracting types & nested types

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,8 @@ use clap::Parser;
 use std::io::prelude::*;
 use std::path::PathBuf;
 
-use crate::utils::{
-    get_files,
-    create_file_in_folder,
-    read_content,
-    add_export_keyword
-};
+use crate::utils::{add_export_keyword, create_file_in_folder, get_files, read_content, get_block_of_code};
 mod utils;
-
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -28,7 +22,7 @@ fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
     let mut output_file = create_file_in_folder(&args.to, &args.file)?;
-    let output_file_content =
+    let output_file_content: String =
         read_content(PathBuf::from(&args.to).join(&args.file).to_str().unwrap());
 
     let files = get_files(&args.from);
@@ -36,8 +30,7 @@ fn main() -> std::io::Result<()> {
         let content = read_content(&file_path);
         let blocks = get_block_of_code(&content);
 
-        println!("Blocks: {:?}", blocks);
-        content.lines().for_each(|line| {
+        blocks.lines().for_each(|line| {
             if !output_file_content.contains(line) {
                 let new_line = add_export_keyword(line);
                 if !new_line.trim().is_empty() {
@@ -49,27 +42,4 @@ fn main() -> std::io::Result<()> {
 
     Ok(())
 }
-
-
-fn get_block_of_code(content: &str) -> Vec<String> {
-    let mut block_of_code = Vec::new();
-    let mut is_block = false;
-
-    for line in content.lines() {
-        if line.contains("type") || line.contains("interface") {
-            is_block = true;
-        }
-
-        if is_block {
-            block_of_code.push(line.to_string());
-        }
-
-        if line.contains("}") {
-            is_block = false;
-        }
-    }
-
-    block_of_code
-}
-
 
